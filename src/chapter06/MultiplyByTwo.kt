fc34b9d73f1837ff40a178f67434d13570d8b105
kimdaegwon <daegwonkim.dev@gmail.com>
2025-10-11 17:04:04 +0900

    chapter06 practice

---
+++ src/chapter06/MultiplyByTwo.kt
@@ -0,0 +1,11 @@
+package chapter06
+
+fun multiplyByTwo(x: Int): Int {
+    println("Inside multiplyByTwo")
+    return x * 2
+}
+
+fun main() {
+    val r = multiplyByTwo(5 )
+    println(r)
+}
---
+++ src/chapter06/SayHello.kt
@@ -0,0 +1,14 @@
+package chapter06
+
+fun sayHello() {
+    println("Hello!")
+}
+
+fun sayGoodBye(): Unit {
+    println("Auf Wiedersehen!")
+}
+
+fun main() {
+    sayHello()
+    sayGoodBye()
+}
