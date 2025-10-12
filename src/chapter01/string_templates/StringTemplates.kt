cd702c3fef545672143d5f9af72c4244d8b3123a
kimdaegwon <daegwonkim.dev@gmail.com>
2025-10-12 15:57:10 +0900

    chapter01 practice

---
+++ src/chapter01/string_templates/StringTemplates.kt
@@ -0,0 +1,7 @@
+package chapter01.string_templates
+
+fun main() {
+    val answer = 42
+    println("Found $answer!")
+    println("printing a $1")
+}
---
+++ src/chapter01/string_templates/TripleQuotes.kt
@@ -0,0 +1,7 @@
+package chapter01.string_templates
+
+fun main() {
+    val s = "value"
+    println("s = \"$s\".")
+    println("""s = "$s".""")
+}
