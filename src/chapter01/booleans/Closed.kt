dd4942e593795bd8300c3c9ea29aeb54f56d1382
kimdaegwon <daegwonkim.dev@gmail.com>
2025-10-12 16:37:26 +0900

    chapter01: Booleans

---
+++ src/chapter01/booleans/Closed.kt
@@ -0,0 +1,11 @@
+package chapter01.booleans
+
+fun isClosed(hour: Int) {
+    val open = 9
+    val closed = 20
+    println("Operating hours: $open - $closed")
+    val status = hour < open || hour > closed
+    println("Closed: $status")
+}
+
+fun main() = isClosed(6)
---
+++ src/chapter01/booleans/Open2.kt
@@ -0,0 +1,11 @@
+package chapter01.booleans
+
+fun isOpen2(hour: Int) {
+    val open = 9
+    val closed = 20
+    println("Operating hours: $open - $closed")
+    val status = hour >= open && hour <= closed
+    println("Open: $status")
+}
+
+fun main() = isOpen2(6)
