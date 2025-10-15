f49e14d0ddce34d41b969960881b62d0534b1047
kimdaegwon <daegwonkim.dev@gmail.com>
2025-10-15 15:30:22 +0900

    chapter04: OperationsOnCollections

---
+++ src/main/kotlin/chapter04/operations_on_collections/ByOperations.kt
@@ -0,0 +1,20 @@
+package chapter04.operations_on_collections
+
+import atomictest.eq
+
+data class Product(
+    val description: String,
+    val price: Double
+)
+
+fun main() {
+    val products = listOf(
+        Product("bread", 2.0),
+        Product("wine", 5.0)
+    )
+    products.sumOf { it.price } eq 7.0
+
+    products.sortedByDescending { it.price } eq
+            "[Product(description=wine, price=5.0), Product(description=bread, price=2.0)]"
+    products.minByOrNull { it.price } eq Product("bread", 2.0)
+}
