15ff257b87ed9c63995e1e4b93c2f4257bcb81a4
kimdaegwon <daegwonkim.dev@gmail.com>
2025-10-15 14:59:52 +0900

    chapter04: ImportanceOfLambdas

---
+++ src/main/kotlin/chapter04/importance_of_lambdas/FunctionClosure.kt
@@ -0,0 +1,14 @@
+package chapter04.importance_of_lambdas
+
+import atomictest.eq
+
+var x = 100
+
+fun useX() {
+    x++
+}
+
+fun main() {
+    useX()
+    x eq 101
+}
---
+++ src/main/kotlin/chapter04/importance_of_lambdas/Sum.kt
@@ -0,0 +1,10 @@
+package chapter04.importance_of_lambdas
+
+import atomictest.eq
+
+fun main() {
+    val list = listOf(1, 5, 7, 10)
+    val divider = 5
+    list.filter { it % divider == 0 }
+        .sum() eq 15
+}
