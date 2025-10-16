d6efcd021916152ec7b7c1a7214d4c51f6d09bf1
kimdaegwon <daegwonkim.dev@gmail.com>
2025-10-16 13:33:12 +0900

    chapter05: SecondaryConstructors

---
+++ src/main/kotlin/chapter05/secondary_constructors/GardenItem.kt
@@ -0,0 +1,30 @@
+package chapter05.secondary_constructors
+
+import atomictest.eq
+import chapter05.secondary_constructors.Material.*
+
+enum class Material {
+    Ceramic, Metal, Plastic
+}
+
+class GardenItem(val name: String) {
+    var material: Material = Plastic
+    constructor(
+        name: String, material: Material
+    ) : this(name) {
+        this.material = material
+    }
+    constructor(
+        material: Material
+    ) : this("Strange Thing", material)
+    override fun toString() = "$material $name"
+}
+
+fun main() {
+    GardenItem("Elf").material eq Plastic
+    GardenItem("Snowman").name eq "Snowman"
+    GardenItem("Gazing Ball", Metal) eq
+            "Metal Gazing Ball"
+    GardenItem(material = Ceramic) eq
+            "Ceramic Strange Thing"
+}
