a5ddcb6f946ed670a1cb9d51371850886722250e
kimdaegwon <daegwonkim.dev@gmail.com>
2025-10-17 14:00:20 +0900

    chapter06: CheckInstructions

---
+++ src/main/kotlin/chapter06/check_instructions/SingleArgRequire.kt
@@ -0,0 +1,15 @@
+package chapter06.check_instructions
+
+import atomictest.*
+
+fun singleArgRequire(arg: Int): Int {
+    require(arg > 5)
+    return arg
+}
+
+fun main() {
+    capture {
+        singleArgRequire(5)
+    } eq "IllegalArgumentException: Failed requirement."
+    singleArgRequire(6) eq 6
+}
